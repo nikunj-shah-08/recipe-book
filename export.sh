bb7821bbb126cb46b462f008e3329768c5658052
Nikunj Shah <nikunj+github@keshah.us>
2026-01-21 23:35:56 -0500

    Rename directory for GH Pages

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 # Export all recipes in a directory
 for recipe in source/*.cook; do
   basename=$(basename "$recipe" .cook)
-  cook recipe -f markdown "$recipe" > "formatted/${basename}.md"
+  cook recipe -f markdown "$recipe" > "docs/${basename}.md"
 done
